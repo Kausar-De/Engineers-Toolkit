8aab50d3e799eb1f0ab815375090f2323f7ab2f5
Kausar-De <71579656+Kausar-De@users.noreply.github.com>
2020-10-31 09:04:14 +0530

    Added Code folder, as well as main code body
    Currently code body only has framework to support different pages, a starting page and a page that lets users access different parts of the calculator.

---
+++ Code/ScientificCalcMAIN.py
@@ -0,0 +1,102 @@
+import tkinter as tk #For GUI
+import tkinter.ttk as ttk #For styling
+from PIL import ImageTk, Image #For image processing
+import numpy as np #For matrix operations
+import math #For math expressions
+
+LargeFont = ('Verdana', 12) #Standard large font to be used throughout
+TitleFont = ('Times New Roman', 20, 'bold') #Standard font to be used for headline text
+LabelFont = ('Arial', 13, 'bold') #Standard font to be used for labels
+
+uHist = [] #Usage History array
+
+expression = '' #Global variable declarations
+uStatement = ''
+evar = math.e
+pivar = math.pi
+phivar = 1.618033988749894
+
+class Calculator(tk.Tk): #Main program class w/ container
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title(self, 'Scientific Calculator') #Program title
+        
+        container = tk.Frame(self) #Code for main container
+        container.pack(side = 'top', fill = 'both', expand = True)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+
+        self.frames = {} #Code for accomodating different frames in which code will run
+
+        for F in (StartPage, ChoicePage): #Iterates through the different pages. Different page names will be added here as they are created
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row = 0, column = 0, sticky = 'nsew')
+
+        self.show_frame(StartPage) #Shows the starting page
+
+    def show_frame(self, cont): #This function is used to display the different frames
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame): #This class is for the starting page, which will be displayed first
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent, bg = 'light blue') #Defines the frame, as well as inherits from container class
+
+        startlf = tk.LabelFrame(self, bg = 'light blue', borderwidth = 0, highlightthickness = 0) #This label enables positioning buttons centrally
+        startlf.grid(row = 2, column = 1, padx = 420, pady = 10)
+
+        label1 = tk.Label(self, text = 'Welcome to the Calculator!', font = TitleFont, fg = 'maroon', bg = 'light green') #These labels are for displaying the title text
+        label1.grid(row = 0, column = 1, padx = 250, pady = 10) #To position label
+        label2 = tk.Label(self, text = 'Please press START to continue or QUIT to exit!', font = TitleFont, fg = 'maroon', bg = 'light green')
+        label2.grid(row = 1, column = 1, padx = 150, pady = 10)
+
+        s = ttk.Style() #Button styling
+        s.configure('btn.TButton', font = ('Arial', 12), background = 'light green')
+        s.map('btn.TButton', foreground = [('active', '!disabled', 'maroon')], background = [('active', 'black')])
+
+        startbutton = ttk.Button(startlf, text = 'START', style = 'btn.TButton', command = lambda: controller.show_frame(ChoicePage)) #This button redirects to ChoicePage
+        startbutton.grid(row = 0, column = 0, padx = 10, pady = 100, sticky = 'e')
+
+        quitbutton = ttk.Button(startlf, text = 'QUIT', style = 'btn.TButton', command = lambda: quit()) #This button ends the program
+        quitbutton.grid(row = 0, column = 1, padx = 10, pady = 100, sticky = 'w')
+                                
+class ChoicePage(tk.Frame): #This class is for the second page, where user can choose which part of calculator to use
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent, bg = 'light blue')
+
+        choiceslf = tk.LabelFrame(self, bg = 'light blue', borderwidth = 0, highlightthickness = 0) #This label enables positioning buttons centrally
+        choiceslf.grid(row = 1, column = 0, padx = 360, pady = 100)
+
+        label = tk.Label(self, text = 'So, what are we doing?', font = TitleFont, fg = 'maroon', bg = 'light green') #Title label
+        label.grid(row = 0, column = 0, padx = 150, pady = 10, sticky = 'nsew') 
+
+        arithbutton = ttk.Button(choiceslf, text = 'Arithmetic', style = 'btn.TButton') #Button to take us to arithmetic operations (PLANNED)
+        arithbutton.grid(row = 0, column = 0, padx = 10, pady = 20)
+
+        numpybutton = ttk.Button(choiceslf, text = 'Matrices & More', style = 'btn.TButton') #Button to take us to numpy operations (PLANNED)
+        numpybutton.grid(row = 0, column = 1, padx = 10, pady = 20)
+
+        graphbutton = ttk.Button(choiceslf, text = 'Graphs', style = 'btn.TButton') #Button to take us to matplotlib operations (PLANNED)
+        graphbutton.grid(row = 1, column = 0, padx = 10, pady = 20)
+
+        docsbutton = ttk.Button(choiceslf, text = 'Useful Formulae', style = 'btn.TButton') #Button to take us to documentations page (PLANNED)
+        docsbutton.grid(row = 1, column = 1, padx = 10, pady = 20)
+
+        backbutton = ttk.Button(choiceslf, text = 'Back', style = 'btn.TButton', command = lambda: controller.show_frame(StartPage)) #This button takes us to the previous page
+        backbutton.grid(row = 0, column = 2, padx = 10, pady = 20)
+
+        histbutton = ttk.Button(choiceslf, text = 'History', style = 'btn.TButton') #This button takes us to the usage history page (PLANNED)
+        histbutton.grid(row = 1, column = 2, padx = 10, pady = 10)
+
+root = Calculator()
+
+root.mainloop()
