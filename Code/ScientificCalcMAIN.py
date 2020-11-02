cee4fb74d59a6bf312528ac0210cb0b64215e4bc
Kausar-De <71579656+Kausar-De@users.noreply.github.com>
2020-11-02 08:36:57 +0530

    Update ScientificCalcMAIN.py
    Added usage history and arithmetic page

--- Code/ScientificCalcMAIN.py
+++ Code/ScientificCalcMAIN.py
@@ -12,6 +12,7 @@ uHist = [] #Usage History array
 
 expression = '' #Global variable declarations
 uStatement = ''
+prec = 2
 evar = math.e
 pivar = math.pi
 phivar = 1.618033988749894
@@ -31,7 +32,7 @@ class Calculator(tk.Tk): #Main program class w/ container
 
         self.frames = {} #Code for accomodating different frames in which code will run
 
-        for F in (StartPage, ChoicePage): #Iterates through the different pages. Different page names will be added here as they are created
+        for F in (StartPage, ChoicePage, ArithPage, HistPage): #Iterates through the different pages. Different page names will be added here as they are created
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky = 'nsew')
@@ -111,7 +112,7 @@ class ChoicePage(tk.Frame): #This class is for the second page, where user can c
         label = tk.Label(self, text = 'So, what are we doing?', font = TitleFont, fg = '#00adb5', bg = '#222831') #Title label
         label.grid(row = 0, column = 0, padx = 150, pady = 10, sticky = 'nsew') 
 
-        arithbutton = ttk.Button(choiceslf, text = 'Arithmetic', style = 'btn.TButton') #Button to take us to arithmetic operations (PLANNED)
+        arithbutton = ttk.Button(choiceslf, text = 'Arithmetic', style = 'btn.TButton', command = lambda: controller.show_frame(ArithPage)) #Button to take us to arithmetic operations (PLANNED)
         arithbutton.grid(row = 0, column = 0, padx = 10, pady = 20)
 
         numpybutton = ttk.Button(choiceslf, text = 'Matrices & More', style = 'btn.TButton') #Button to take us to numpy operations (PLANNED)
@@ -126,8 +127,184 @@ class ChoicePage(tk.Frame): #This class is for the second page, where user can c
         backbutton = ttk.Button(choiceslf, text = 'Back', style = 'btn.TButton', command = lambda: controller.show_frame(StartPage)) #This button takes us to the previous page
         backbutton.grid(row = 0, column = 2, padx = 10, pady = 20)
 
-        histbutton = ttk.Button(choiceslf, text = 'History', style = 'btn.TButton') #This button takes us to the usage history page (PLANNED)
+        histbutton = ttk.Button(choiceslf, text = 'History', style = 'btn.TButton', command = lambda: controller.show_frame(HistPage)) #This button takes us to the usage history page (PLANNED)
         histbutton.grid(row = 1, column = 2, padx = 10, pady = 10)
+
+class ArithPage(tk.Frame): #This class is for the Arithmetic operations page
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent, bg = '#222831')
+
+        def click(num): #This functions enables button inputs
+            global prec
+            global expression
+            
+            expression = expression + str(num) #Num argument is substituted by input from calculator buttons
+            equation.set(expression)
+
+        def solve(): #This function equates the inputted expression
+            try:
+                global prec
+                global expression
+                global uStatement
+                global uHist
+                
+                ans = str(round(eval(expression), prec)) #Using eval to calculate user inputed expression and is then converted to string
+                uStatement = str(expression + ' = ' + ans + '\n') #Usage history statement
+                uHist.append(uStatement)
+                equation.set(ans) #Displays answer in inputfield
+                expression = ans
+            except:
+                equation.set('ERROR')
+                expression = ''
+
+        def clr(): #This function is for behaviour of CLEAR button for arithmetic
+            global expression
+            expression = ''
+            equation.set('Enter your Input')
+
+        equation = tk.StringVar() #These variables are the text variables for all the Entry fields
+        equation.set('Enter your Input')
+
+        label = tk.Label(self, text = 'Arithmetic', font = TitleFont, fg = '#00adb5', bg = '#222831') #Title Label
+        label.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+        arilf = tk.LabelFrame(self, text = 'Arithmetic:', font = LabelFont, fg = '#00adb5', bg = '#393e46') #This label frame contains all the stuff for simple arithmetic
+        arilf.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+        inputfield = tk.Entry(arilf, textvariable = equation, font = LargeFont, state = 'disabled') #This entry field displays user input and output
+        inputfield.grid(columnspan = 4, ipadx = 20, ipady = 3, pady = 15)
+
+        btn1 = ttk.Button(arilf, text = '1', style = 'btn.TButton', command = lambda: click(1)) #Calculator buttons
+        btn1.grid(row = 5, column = 0, padx = 10, pady = 10)
+
+        btn2 = ttk.Button(arilf, text = '2', style = 'btn.TButton', command = lambda: click(2))
+        btn2.grid(row = 5, column = 1, padx = 10, pady = 10)
+
+        btn3 = ttk.Button(arilf, text = '3', style = 'btn.TButton', command = lambda: click(3))
+        btn3.grid(row = 5, column = 2, padx = 10, pady = 10)
+
+        btn4 = ttk.Button(arilf, text = '4', style = 'btn.TButton', command = lambda: click(4))
+        btn4.grid(row = 4, column = 0, padx = 10, pady = 10)
+
+        btn5 = ttk.Button(arilf, text = '5', style = 'btn.TButton', command = lambda: click(5))
+        btn5.grid(row = 4, column = 1, padx = 10, pady = 10)
+
+        btn6 = ttk.Button(arilf, text = '6', style = 'btn.TButton', command = lambda: click(6))
+        btn6.grid(row = 4, column = 2, padx = 10, pady = 10)
+
+        btn7 = ttk.Button(arilf, text = '7', style = 'btn.TButton', command = lambda: click(7))
+        btn7.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+        btn8 = ttk.Button(arilf, text = '8', style = 'btn.TButton', command = lambda: click(8))
+        btn8.grid(row = 3, column = 1, padx = 10, pady = 10)
+
+        btn9 = ttk.Button(arilf, text = '9', style = 'btn.TButton', command = lambda: click(9))
+        btn9.grid(row = 3, column = 2, padx = 10, pady = 10)
+
+        btn0 = ttk.Button(arilf, text = '0', style = 'btn.TButton', command = lambda: click(0))
+        btn0.grid(row = 6, column = 1, padx = 10, pady = 10)
+
+        btndec = ttk.Button(arilf, text = '.', style = 'btn.TButton', command = lambda: click('.'))
+        btndec.grid(row = 6, column = 0, padx = 10, pady = 10)
+
+        btneql = ttk.Button(arilf, text = '=', style = 'btn.TButton', command = lambda: solve())
+        btneql.grid(row = 6, column = 2, padx = 10, pady = 10)
+
+        btndiv = ttk.Button(arilf, text = '/', style = 'btn.TButton', command = lambda: click('/'))
+        btndiv.grid(row = 3, column = 3, padx = 10, pady = 10)
+
+        btnmult = ttk.Button(arilf, text = '*', style = 'btn.TButton', command = lambda: click('*'))
+        btnmult.grid(row = 4, column = 3, padx = 10, pady = 10)
+
+        btnmin = ttk.Button(arilf, text = '-', style = 'btn.TButton', command = lambda: click('-'))
+        btnmin.grid(row = 5, column = 3, padx = 10, pady = 10)
+
+        btnplus = ttk.Button(arilf, text = '+', style = 'btn.TButton', command = lambda: click('+'))
+        btnplus.grid(row = 6, column = 3, padx = 10, pady = 10)
+
+        btnlbr = ttk.Button(arilf, text = '(', style = 'btn.TButton', command = lambda: click('('))
+        btnlbr.grid(row = 1, column = 2, padx = 10, pady = 10)
+
+        btnrbr = ttk.Button(arilf, text = ')', style = 'btn.TButton', command = lambda: click(')'))
+        btnrbr.grid(row = 1, column = 3, padx = 10, pady = 10)
+
+        btne = ttk.Button(arilf, text = 'e', style = 'btn.TButton', command = lambda: click(round(evar, prec)))
+        btne.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        btnpi = ttk.Button(arilf, text = '\u03C0', style = 'btn.TButton', command = lambda: click(round(pivar, prec)))
+        btnpi.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+        btnexp = ttk.Button(arilf, text = '**', style = 'btn.TButton', command = lambda: click('**'))
+        btnexp.grid(row = 2, column = 2, padx = 10, pady = 10)
+
+        btnflrdiv = ttk.Button(arilf, text = '//', style = 'btn.TButton', command = lambda: click('//'))
+        btnflrdiv.grid(row = 2, column = 3, padx = 10, pady = 10)
+
+        btng = ttk.Button(arilf, text = 'g', style = 'btn.TButton', command = lambda: click(9.8))
+        btng.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+        btnphi = ttk.Button(arilf, text = '\u03C6', style = 'btn.TButton', command = lambda: click(round(phivar, prec)))
+        btnphi.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        btnclr = ttk.Button(arilf, text = 'Clear', style = 'btn.TButton', command = lambda: clr()) #Button to clear inputfield
+        btnclr.grid(row = 0, column = 3, padx = 10, pady = 10)
+        
+        modabslf = tk.LabelFrame(self, text = 'Modulus & Absolute:', font = LabelFont, fg = '#00adb5', bg = '#393e46') #This label frame contains everything else
+        modabslf.grid(row = 1, column = 2, padx = 10, pady = 10)
+
+        modlabel = tk.Label(modabslf, text = 'Modulus:', font = LabelFont, fg = '#00adb5', bg = '#222831') #Modulus Label
+        modlabel.grid(row = 0, column = 0, padx = 10, pady = 7.5, sticky = 'w')
+
+        backbutton = ttk.Button(self, text = 'Back', style = 'btn.TButton', command = lambda: controller.show_frame(ChoicePage)) #This button takes us to the previous page
+        backbutton.grid(row = 0, column = 2, padx = 10, sticky = 'e')
+
+        histbutton = ttk.Button(self, text = 'History', style = 'btn.TButton', command = lambda: controller.show_frame(HistPage)) #This button takes us to the history page
+        histbutton.grid(row = 0, column = 0, padx = 10, sticky = 'w')
+
+class HistPage(tk.Frame): #This class is for the History page
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent, bg = '#222831')
+
+        label = tk.Label(self, text = 'Usage History', font = TitleFont, fg = '#00adb5', bg = '#222831') #Title label
+        label.grid(row = 0, column = 1, padx = 350, pady = 10, sticky = 'nsew')
+
+        def showHist(): #This function is to print usage history in text field
+            try:
+                global uHist
+                
+                n = len(uHist)
+                element = ''
+                for i in reversed(range(n)):
+                    element = str(element + uHist[i] + '\n')
+                history.insert(tk.END, element)            
+            except:
+                print("\nSomething went wrong!")
+
+        def clrpage(): #This function is to clear the contents on usage history text field
+            history.delete('1.0', 'end')
+
+        def clrhist(): #This function is to delete the usage history
+            global uHist
+            uHist.clear()
+
+        history = tk.Text(self, font = LargeFont, fg = '#00adb5', bg = '#393e46', height = 20, width = 80)
+        history.grid(row = 1, column = 1, padx = 40, pady = 10)
+
+        dispbutton = ttk.Button(self, text = 'Show', style = 'btn.TButton', command = lambda: showHist()) #This button displays usage history
+        dispbutton.grid(row = 0, column = 0, padx = 10, sticky = 'w')
+
+        clrtextbtn = ttk.Button(self, text = 'Clear Page', style = 'btn.TButton', command = lambda: clrpage()) #This button clears the history page
+        clrtextbtn.grid(row = 1, column = 2, padx = 10, sticky = 'e')
+
+        clrhistbtn = ttk.Button(self, text = 'Clear History', style = 'btn.TButton', command = lambda: clrhist()) #This button deletes usage history
+        clrhistbtn.grid(row = 1, column = 2, padx = 10, sticky = 'ne')
+
+        choicebutton = ttk.Button(self, text = 'Choices', style = 'btn.TButton', command = lambda: controller.show_frame(ChoicePage)) #This button takes us to the choices page
+        choicebutton.grid(row = 0, column = 2, padx = 10, sticky = 'e')
 
 root = Calculator()
 
